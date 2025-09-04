9cf8656f18a0a2a203864e72ce3466eb027c5560
akshay-2104 <akshaypatil21042104@gmail.com>
2025-09-04 18:14:11 +0530

    Error Solved

--- src/main/java/com/akshay/CareerNest/repository/JobRepository.java
+++ src/main/java/com/akshay/CareerNest/repository/JobRepository.java
@@ -1,7 +1,6 @@
 package com.akshay.CareerNest.repository;
 
 import com.akshay.CareerNest.entity.JobPost;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface JobRepository extends MongoRepository<JobPost, String> {
