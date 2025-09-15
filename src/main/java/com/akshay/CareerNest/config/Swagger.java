98a11ceb3043d960bc08a3b7c83c98666132a430
akshay-2104 <akshaypatil21042104@gmail.com>
2025-09-15 18:17:55 +0530

    Swagger Implemented

--- src/main/java/com/akshay/CareerNest/config/Swagger.java
+++ src/main/java/com/akshay/CareerNest/config/Swagger.java
@@ -2,6 +2,8 @@ package com.akshay.CareerNest.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +22,14 @@ public OpenAPI myCustomConfig()
                    )
            .servers(List.of(new Server().url("http://localhost:8080").description("local"),
                    new Server().url("http://localhost:8082").description("live"))
+           )
+           .addSecurityItem(new SecurityRequirement().addList("basicAuth"))
+           .components(new io.swagger.v3.oas.models.Components()
+                   .addSecuritySchemes("basicAuth",
+                           new SecurityScheme()
+                                   .type(SecurityScheme.Type.HTTP)
+                                   .scheme("basic")
+                   )
            );
 
 
